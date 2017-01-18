a08a345bd98426387b5065e18fa622a72fc133f3
Terry Yin <terry.yinzhe@gmail.com>
2017-01-18 14:07:50 +0800

    secret:

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = 'c36584e4148cf6852300393e1e03e62d82d2795ae91c3c987d29aef8b448cc765f619afe0b955bf18ac3d930df6dfcf3f24fcb83c4ef6821bba4f54cd359f22c'
+Dummy::Application.config.secret_key_base = '964cc165a5ebf653f2ce3d21c2f95af14f4c2621f0b05bf1ea4e2d030d91da4cc1c753881b1d63feae377b016d23a686bec947445c5085ba2f661631d41b85bb'
